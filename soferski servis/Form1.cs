81f1b1e0d0b8980b065cb5a78a964acb1ed00450
Filip Curcic <filipcurcic42@gmail.com>
2018-04-24 21:08:32 +0200

    First additions

--- soferski servis/Form1.cs
+++ soferski servis/Form1.cs
@@ -16,5 +16,47 @@ namespace soferski_servis
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void addToCb()
+        {
+            comboBox1.Items.Add("aasd");
+            comboBox1.Items.Add("aas");
+            comboBox1.Items.Add("ggggd");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            addToCb();
+        }
+        private string addToTb()
+        {
+            string tekst = comboBox1.SelectedItem.ToString();
+            return tekst;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = addToTb();
+        }
     }
 }
