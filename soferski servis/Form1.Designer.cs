d93b104ea1dae1bc38f5df5ba4ba7fe384489c83
Filip Curcic <filipcurcic42@gmail.com>
2018-04-26 15:35:49 +0200

    Tweaked UI and fixed query execution

--- soferski servis/Form1.Designer.cs
+++ soferski servis/Form1.Designer.cs
@@ -41,7 +41,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe MDL2 Assets", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(242, 9);
+            this.label1.Location = new System.Drawing.Point(14, -6);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(322, 48);
             this.label1.TabIndex = 0;
@@ -51,47 +51,47 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 121);
+            this.comboBox1.Location = new System.Drawing.Point(10, 126);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(631, 21);
+            this.comboBox1.Size = new System.Drawing.Size(224, 21);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 90);
+            this.label2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 95);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(156, 28);
+            this.label2.Size = new System.Drawing.Size(151, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "Choose criteria";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 192);
+            this.textBox1.Location = new System.Drawing.Point(10, 197);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(776, 246);
+            this.textBox1.Size = new System.Drawing.Size(323, 246);
             this.textBox1.TabIndex = 3;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 161);
+            this.label3.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 166);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 28);
+            this.label3.Size = new System.Drawing.Size(78, 25);
             this.label3.TabIndex = 4;
             this.label3.Text = "Results";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(649, 121);
+            this.button1.Location = new System.Drawing.Point(248, 126);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 23);
+            this.button1.Size = new System.Drawing.Size(85, 23);
             this.button1.TabIndex = 5;
             this.button1.Text = "Generate";
             this.button1.UseVisualStyleBackColor = true;
@@ -100,19 +100,18 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(338, 148);
+            this.button2.Location = new System.Drawing.Point(88, 60);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(139, 23);
+            this.button2.Size = new System.Drawing.Size(146, 23);
             this.button2.TabIndex = 6;
-            this.button2.Text = "Generate";
+            this.button2.Text = "Create Tables";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(345, 455);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
--- soferski servis/Form1.cs
+++ soferski servis/Form1.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace soferski_servis
 {
     public partial class Form1 : Form
     {
+        static string MySqlConnectionString = "datasource=localhost;port=3306;username=root;password=;database=123";
+        MySqlConnection databaseConnection = new MySqlConnection(MySqlConnectionString);
         public Form1()
         {
             InitializeComponent();
+            onloadCb();
+           
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -31,18 +37,97 @@ namespace soferski_servis
         {
             
         }
-        private void addToCb()
+        private void createTables()
         {
-            comboBox1.Items.Add("aasd");
-            comboBox1.Items.Add("aas");
-            comboBox1.Items.Add("ggggd");
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        
+
+        public void runQuery(string query1)
         {
-            addToCb();
+            
+            MySqlCommand commandDatabase = new MySqlCommand(query1, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            databaseConnection.Open();
+            MySqlDataReader myReader = commandDatabase.ExecuteReader();
         }
+        
+        public void button1_Click(object sender, EventArgs e)
+        {
+            int index = comboBox1.SelectedIndex;
+
+            string q;
+            switch (index)
+            {
+                case 0:
+                    q = "CREATE TABLE asd (id INT);";
+                    runQuery(q);
+                    break;
+                case 1:
+                    q = "CREATE TABLE k2 (kejs2 INT);";
+                    runQuery(q);
+                    break;
+                case 2:
+                    textBox1.Text = "3";
+                    break;
+                case 3:
+                    textBox1.Text = "4";
+                    break;
+                case 4:
+                    textBox1.Text = "5";
+                    break;
+                case 5:
+                    textBox1.Text = "6";
+                    break;
+                case 6:
+                    textBox1.Text = "7";
+                    break;
+                case 7:
+                    textBox1.Text = "8";
+                    break;
+                case 8:
+                    textBox1.Text = "9";
+                    break;
+                case 9:
+                    textBox1.Text = "10";
+                    break;
+                case 10:
+                    textBox1.Text = "11";
+                    break;
+                case 11:
+                    textBox1.Text = "12";
+                    break;
+                case 12:
+                    textBox1.Text = "13";
+                    break;
+                case 13:
+                    textBox1.Text = "14";
+                    break;
+                case 14:
+                    textBox1.Text = "15";
+                    break;
+                case 15:
+                    textBox1.Text = "16";
+                    break;
+                case 16:
+                    textBox1.Text = "17";
+                    break;
+                case 17:
+                    textBox1.Text = "18";
+                    break;
+                case 18:
+                    textBox1.Text = "19";
+                    break;
+                case 19:
+                    textBox1.Text = "20";
+                    break;
+                
+
+            }
+            
+        }
+
         private string addToTb()
         {
             string tekst = comboBox1.SelectedItem.ToString();
@@ -58,5 +143,31 @@ namespace soferski_servis
         {
             textBox1.Text = addToTb();
         }
+        private void onloadCb()
+        {
+    
+            comboBox1.Items.Add("1. Šofer sa najviše obavljenih tura u prethodnom mesecu poslovanja");
+            comboBox1.Items.Add("2. Šofer sa najboljim prosecnim rejtingom na godisnjim nivou");
+            comboBox1.Items.Add("3. Tura sa najvećim prosečnim brojem putnika u letnjem periodu");
+            comboBox1.Items.Add("4. Tura koja donosi najveći profit");
+            comboBox1.Items.Add("5. Lokacija sa najvećim padom popularnosti u poslednjih 3 godine");
+            comboBox1.Items.Add("6.  Šofer sa najboljim odnosom potrošenog goriva i pređene kilometraže");
+            comboBox1.Items.Add("7. Vozilo sa najvecim troskovima servis u poslednji godinu dana");
+            comboBox1.Items.Add("8. Najmanje popularno vozilo kod putnika mladjih od 25 godina u letnjem periodu");
+            comboBox1.Items.Add("9.  Nacin placanja koji donosi najmanji profit");
+            comboBox1.Items.Add("10.  Najpopularniji nacin placanja kod putnika ispod 30 godina");
+            comboBox1.Items.Add("11. Vozilo sa najvećim porastom kilometraže u poslednjoj godini poslovanja");
+            comboBox1.Items.Add("12. Starost putnika sa najmanje zastupljenim premium clanstvom  ");
+            comboBox1.Items.Add("13. Vozilo sa najmanjim odnosom potrošenog goriva i prihoda po turi");
+            comboBox1.Items.Add("14. Soferi koji imaju bolje prosecne ocene od putnika ove godine u poredjenju sa proslom");
+            comboBox1.Items.Add("15.  Promena profita u odnosu na prethodnu godinu poslovanja");
+            comboBox1.Items.Add("16. Premium putnik sa najvecim rastom predjenog puta u poslednjih 6 meseci");
+            comboBox1.Items.Add("17. Šofer sa najvećim padom rejtinga u poslednjih 3 meseca poslovanja");
+            comboBox1.Items.Add("18. Vozilo sa najvećim prosečnim brojem putnika po turi ");
+            comboBox1.Items.Add("19. 3 najčešće ture sa 3 ili više putnika u vozilu");
+            comboBox1.Items.Add("20. Koji mesec u godini donosi najveci profit");
+        }
+
+        
     }
 }
